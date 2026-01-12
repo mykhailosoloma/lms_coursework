3057d7ee532b81f3cb95fba7daf94243024edfc3
Mykhailo <solomkamisha516@gmail.com>
2026-01-12 13:28:31 +0200

    Initial commit

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,55 @@
+CREATE TABLE users (
+    user_id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password_hash VARCHAR(255),
+    role VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE courses (
+    course_id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    category VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    instructor_id BIGINT,
+    CONSTRAINT fk_course_instructor FOREIGN KEY (instructor_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE modules (
+    module_id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    order_index INT,
+    course_id BIGINT NOT NULL,
+    CONSTRAINT fk_module_course FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE
+);
+
+CREATE TABLE lessons (
+    lesson_id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content_type VARCHAR(50),
+    order_index INT,
+    module_id BIGINT NOT NULL,
+    CONSTRAINT fk_lesson_module FOREIGN KEY (module_id) REFERENCES modules(module_id) ON DELETE CASCADE
+);
+
+CREATE TABLE enrollments (
+    enrollment_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
+    enrolled_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_enrollment_user FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    CONSTRAINT fk_enrollment_course FOREIGN KEY (course_id) REFERENCES courses(course_id) ON DELETE CASCADE,
+    CONSTRAINT uq_student_course UNIQUE (user_id, course_id)
+);
+
+CREATE TABLE progress (
+    progress_id BIGSERIAL PRIMARY KEY,
+    enrollment_id BIGINT NOT NULL,
+    lesson_id BIGINT NOT NULL,
+    status VARCHAR(50) DEFAULT 'NOT_STARTED',
+    last_accessed TIMESTAMP,
+    CONSTRAINT fk_progress_enrollment FOREIGN KEY (enrollment_id) REFERENCES enrollments(enrollment_id) ON DELETE CASCADE,
+    CONSTRAINT fk_progress_lesson FOREIGN KEY (lesson_id) REFERENCES lessons(lesson_id) ON DELETE CASCADE
---
+++ src/test/java/edu/kpi/lms_system/LmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kpi.lms_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
